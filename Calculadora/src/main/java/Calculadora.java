6dc371127d1b97c429d2dca0ff203c149b57a2df
thiagobohn <thiagobc.sp@hotmail.com>
2022-05-15 16:21:17 -0300

    Adicionados comentários no proeto da Calculadora

--- Calculadora/src/main/java/Calculadora.java
+++ Calculadora/src/main/java/Calculadora.java
@@ -7,32 +7,33 @@
  *
  * @author Thiago
  */
-public class Calculadora {
+public class Calculadora { // Projeto criado como exercíco do curso "Entendendo métodos"
 
-    public static void soma (double num1, double num2){
+    public static void soma (double num1, double num2){ //método que implementa a operação de soma de dois valores
         double resultado = num1+num2;
         System.out.println("A soma de " + num1 + " + " + num2 + " é: " + resultado);
     }
     
-    public static void subtracao (double num1, double num2){
+    public static void subtracao (double num1, double num2){ //método que implementa a operação de subtração de dois valores
         double resultado = num1-num2;
         System.out.println("A subtração de " + num1 + " - " + num2 + " é: " + resultado);
     }
     
-    public static void multiplicacao (double num1, double num2){
+    public static void multiplicacao (double num1, double num2){ //método que implementa a operação de multiplicação de dois valores
         double resultado = num1*num2;
         System.out.println("A multiplicação de " + num1 + " vezes " + num2 + " é: " + resultado);
     }
     
-    public static void divisao (double num1, double num2){
+    public static void divisao (double num1, double num2){ //método que implementa a operação de divisão de dois valores
         double resultado = num1/num2;
         System.out.println("A divisão de " + num1 + " por " + num2 + " é: " + resultado);
     }
     
     public static void main(String[] args) {
         double num1 = 7;
-        double num2 = 4;
+        double num2 = 3;
         
+        //utilização/verificação dos métodos implementados
         soma(num1, num2);
         subtracao(num1, num2);
         multiplicacao(num1, num2);

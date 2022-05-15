b14558815ccd4a435b30c87445173a4c70ed85dd
thiagobohn <thiagobc.sp@hotmail.com>
2022-05-15 16:17:02 -0300

    Atualizado o projeto Calculadora com todos os métodos propostos

--- Calculadora/src/main/java/Calculadora.java
+++ Calculadora/src/main/java/Calculadora.java
@@ -13,11 +13,30 @@ public class Calculadora {
         double resultado = num1+num2;
         System.out.println("A soma de " + num1 + " + " + num2 + " é: " + resultado);
     }
+    
+    public static void subtracao (double num1, double num2){
+        double resultado = num1-num2;
+        System.out.println("A subtração de " + num1 + " - " + num2 + " é: " + resultado);
+    }
+    
+    public static void multiplicacao (double num1, double num2){
+        double resultado = num1*num2;
+        System.out.println("A multiplicação de " + num1 + " vezes " + num2 + " é: " + resultado);
+    }
+    
+    public static void divisao (double num1, double num2){
+        double resultado = num1/num2;
+        System.out.println("A divisão de " + num1 + " por " + num2 + " é: " + resultado);
+    }
+    
     public static void main(String[] args) {
         double num1 = 7;
         double num2 = 4;
         
         soma(num1, num2);
+        subtracao(num1, num2);
+        multiplicacao(num1, num2);
+        divisao(num1, num2);
     }
     
 }

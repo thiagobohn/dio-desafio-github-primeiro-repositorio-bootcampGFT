0fcebec2df687825ea30b87074b71cc64144f054
thiagobohn <thiagobc.sp@hotmail.com>
2022-05-17 11:31:31 -0300

    Adicionados comentários no projeto Emprestimo

--- Curso - Entendendo Métodos/Projetos/Emprestimo/src/main/java/Emprestimo.java
+++ Curso - Entendendo Métodos/Projetos/Emprestimo/src/main/java/Emprestimo.java
@@ -12,30 +12,30 @@ public class Emprestimo {
     /**
      * @param args the command line arguments
      */
-    public static int getDuasParcelas() {
+    public static int getDuasParcelas() { // método criado para exemplificar a possibilidade de chamara métodos como parâmetro ao chamar outro método
         return 2;
     }
 
-    public static int getTresParcelas() {
+    public static int getTresParcelas() { // método criado para exemplificar a possibilidade de chamara métodos como parâmetro ao chamar outro método
         return 3;
     }
 
-    public static double getTaxaDuasParcelas() {
+    public static double getTaxaDuasParcelas() { //Método criado para exemplificar que é possível chamar um método dentro do código de outro método
         return 0.3;
     }
 
-    public static double getTaxaTresParcelas(){
+    public static double getTaxaTresParcelas(){ //Método criado para exemplificar que é possível chamar um método dentro do código de outro método
         return 0.45;
     }
     
-    public static void calcular (double valor, int parcelas){
+    public static void calcular (double valor, int parcelas){ //método que calcula o valor final do empréstimo
         if (parcelas == 2){
-            double valorFinal = valor + (valor * getTaxaDuasParcelas());
+            double valorFinal = valor + (valor * getTaxaDuasParcelas()); //Operação que chama o método da taxa de juros utilizado dentro do método que calcula o valor final do empréstimo
             System.out.println("O valor final do emprestimo para duas parcelas é de " + valorFinal);
         }
         else if (parcelas ==3){
-            double valorFinal = valor + (valor * getTaxaTresParcelas());
-            System.out.println("O valor final do emprestimo para três parcelas é de " + valorFinal);
+            double valorFinal = valor + (valor * getTaxaTresParcelas()); //Operação que chama o método da taxa de juros utilizado dentro do método que calcula o valor final do empréstimo
+            System.out.println("O valor final do emprestimo para trêscd /c parcelas é de " + valorFinal);
         }
         else{
             System.out.println("Valor de parcelas não aceita");
@@ -43,9 +43,9 @@ public class Emprestimo {
     }
     
     public static void main(String[] args) {
-        calcular(1000,getDuasParcelas());
-        calcular(1000,getTresParcelas());
-        calcular(1000,5);
+        calcular(1000,getDuasParcelas()); //Chamada do método calcular passando como parâmetro o retorno de um outro método
+        calcular(1000,getTresParcelas()); //Chamada do método calcular passando como parâmetro o retorno de um outro método
+        calcular(1000,5); //Chamada do método calcular para testa um valor de exceção
     }
 
 }
